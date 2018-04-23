c5719728b2f8a9d4e98c6d633cdc0d2150333a00
NoName115 <robert.kolcun@gmail.com>
2018-04-23 12:55:55 +0200

    fix server key exchange sig for TLS1.3, TLS1.2
    remove parentheses to fix travis CI
    
    change version constant

--- tlslite/tlsconnection.py
+++ tlslite/tlsconnection.py
@@ -2523,10 +2523,12 @@ class TLSConnection(TLSRecordLayer):
         # TODO when TLS 1.3 is final, check the client hello random for
         # downgrade too
 
-        scheme = self._pickServerKeyExchangeSig(settings,
-                                                clientHello,
-                                                certChain,
-                                                version)
+        scheme = None
+        if version >= (3, 4):
+            scheme = self._pickServerKeyExchangeSig(settings,
+                                                    clientHello,
+                                                    certChain,
+                                                    version)
 
         #Check if there's intersection between supported curves by client and
         #server
