9fd846573d3f0b17ad4cf2a268aa2585ef626643
Alexander Sosedkin <asosedkin@redhat.com>
2019-12-02 14:33:59 +0100

    Use just a single context for M2Crypto 3DES
    Make M2Crypto 3DES follow suit
    and require separate instances for encryption and decryption.

--- tlslite/utils/openssl_tripledes.py
+++ tlslite/utils/openssl_tripledes.py
@@ -15,24 +15,37 @@ if m2cryptoLoaded:
 
         def __init__(self, key, mode, IV):
             TripleDES.__init__(self, key, mode, IV, "openssl")
+            self._IV, self._key = IV, key
+            self._context = None
+            self._encrypt = None
+
+        def _init_context(self, encrypt=True):
             cipherType = m2.des_ede3_cbc()
-            self.encrypt_context = m2.cipher_ctx_new()
-            self.decrypt_context = m2.cipher_ctx_new()
-            m2.cipher_init(self.encrypt_context, cipherType, key, IV, 1)
-            m2.cipher_init(self.decrypt_context, cipherType, key, IV, 0)
-            m2.cipher_set_padding(self.encrypt_context, 0)
-            m2.cipher_set_padding(self.decrypt_context, 0)
+            self._context = m2.cipher_ctx_new()
+            m2.cipher_init(self._context, cipherType, self._key, self._IV,
+                           int(encrypt))
+            m2.cipher_set_padding(self._context, 0)
+            self._encrypt = encrypt
 
         def encrypt(self, plaintext):
+            if self._context is None:
+                self._init_context(encrypt=True)
+            else:
+                assert self._encrypt, '.encrypt() not allowed after .decrypt()'
             TripleDES.encrypt(self, plaintext)
-            ciphertext = m2.cipher_update(self.encrypt_context, plaintext)
+            ciphertext = m2.cipher_update(self._context, plaintext)
             return bytearray(ciphertext)
 
         def decrypt(self, ciphertext):
+            if self._context is None:
+                self._init_context(encrypt=False)
+            else:
+                assert not self._encrypt, \
+                       '.decrypt() not allowed after .encrypt()'
             TripleDES.decrypt(self, ciphertext)
-            plaintext = m2.cipher_update(self.decrypt_context, ciphertext)
+            plaintext = m2.cipher_update(self._context, ciphertext)
             return bytearray(plaintext)
 
         def __del__(self):
-            m2.cipher_ctx_free(self.encrypt_context)
-            m2.cipher_ctx_free(self.decrypt_context)
+            if self._context is not None:
+                m2.cipher_ctx_free(self._context)
