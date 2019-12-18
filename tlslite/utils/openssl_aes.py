db757e295f9160ad8f187ca1ea22780db7f039e7
Alexander Sosedkin <asosedkin@redhat.com>
2019-12-18 16:54:11 +0100

    Fix IV handling for AES, test consecutive calls
    M2Crypto defaults to padding the ciphertext,
    and the previous implementation danced around that awkwardly
    by padding and unpadding ciphertext on decryption
    and updating the IV manually.
    `m2.cipher_set_padding(context, 0)`
    allows to shoulder the IV handling back to where it belongs
    and to get rid of unnecessary context reinitializations.
    (Investigative work courtesy of @tomato42:
     #377 (review)).
    
    This commit mirrors changes from #377, but for AES
    and adds the same unit test as well.

--- tlslite/utils/openssl_aes.py
+++ tlslite/utils/openssl_aes.py
@@ -9,44 +9,52 @@ from .aes import *
 if m2cryptoLoaded:
 
     def new(key, mode, IV):
+        # IV argument name is a part of the interface
+        # pylint: disable=invalid-name
         return OpenSSL_AES(key, mode, IV)
 
     class OpenSSL_AES(AES):
 
         def __init__(self, key, mode, IV):
+            # IV argument/field names are a part of the interface
+            # pylint: disable=invalid-name
             AES.__init__(self, key, mode, IV, "openssl")
-            self.key = key
-            self.IV = IV
+            self.IV, self._key = IV, key
+            self._context = None
+            self._encrypt = None
 
-        def _createContext(self, encrypt):
-            context = m2.cipher_ctx_new()
-            if len(self.key)==16:
+        def _init_context(self, encrypt=True):
+            if len(self._key) == 16:
                 cipherType = m2.aes_128_cbc()
-            if len(self.key)==24:
+            if len(self._key) == 24:
                 cipherType = m2.aes_192_cbc()
-            if len(self.key)==32:
+            if len(self._key) == 32:
                 cipherType = m2.aes_256_cbc()
-            m2.cipher_init(context, cipherType, self.key, self.IV, encrypt)
-            return context
+            self._context = m2.cipher_ctx_new()
+            m2.cipher_init(self._context, cipherType, self._key, self.IV,
+                           int(encrypt))
+            m2.cipher_set_padding(self._context, 0)
+            self._encrypt = encrypt
 
         def encrypt(self, plaintext):
+            if self._context is None:
+                self._init_context(encrypt=True)
+            else:
+                assert self._encrypt, '.encrypt() not allowed after .decrypt()'
             AES.encrypt(self, plaintext)
-            context = self._createContext(1)
-            ciphertext = m2.cipher_update(context, plaintext)
-            m2.cipher_ctx_free(context)
-            self.IV = ciphertext[-self.block_size:]
+            ciphertext = m2.cipher_update(self._context, plaintext)
             return bytearray(ciphertext)
 
         def decrypt(self, ciphertext):
+            if self._context is None:
+                self._init_context(encrypt=False)
+            else:
+                assert not self._encrypt, \
+                       '.decrypt() not allowed after .encrypt()'
             AES.decrypt(self, ciphertext)
-            context = self._createContext(0)
-            #I think M2Crypto has a bug - it fails to decrypt and return the last block passed in.
-            #To work around this, we append sixteen zeros to the string, below:
-            plaintext = m2.cipher_update(context, ciphertext+(b'\0'*16))
-
-            #If this bug is ever fixed, then plaintext will end up having a garbage
-            #plaintext block on the end.  That's okay - the below code will discard it.
-            plaintext = plaintext[:len(ciphertext)]
-            m2.cipher_ctx_free(context)
-            self.IV = ciphertext[-self.block_size:]
+            plaintext = m2.cipher_update(self._context, ciphertext)
             return bytearray(plaintext)
+
+        def __del__(self):
+            if self._context is not None:
+                m2.cipher_ctx_free(self._context)
