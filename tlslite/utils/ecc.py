2316e6e05072e5afa337324e9fbf62e19fbbec1e
pytz <36339456+pytz@users.noreply.github.com>
2020-10-22 19:37:32 +0200

    Added multiple brainpoolP___r1 to available curves

--- tlslite/utils/ecc.py
+++ tlslite/utils/ecc.py
@@ -37,7 +37,14 @@ def getCurveByName(curveName):
     curveMap = {'secp256r1':ecdsa.NIST256p,
                 'secp384r1':ecdsa.NIST384p,
                 'secp521r1':ecdsa.NIST521p,
-                'secp256k1':ecdsa.SECP256k1}
+                'secp256k1':ecdsa.SECP256k1,
+                'brainpoolP160r1': ecdsa.BRAINPOOLP160r1,
+                'brainpoolP192r1': ecdsa.BRAINPOOLP192r1,
+                'brainpoolP224r1': ecdsa.BRAINPOOLP224r1,
+                'brainpoolP256r1': ecdsa.BRAINPOOLP256r1,
+                'brainpoolP320r1': ecdsa.BRAINPOOLP320r1,
+                'brainpoolP384r1': ecdsa.BRAINPOOLP384r1,
+                'brainpoolP512r1': ecdsa.BRAINPOOLP512r1}
     if ecdsaAllCurves:
         curveMap['secp224r1'] = ecdsa.NIST224p
         curveMap['secp192r1'] = ecdsa.NIST192p
@@ -52,7 +59,14 @@ def getPointByteSize(point):
     curveMap = {ecdsa.NIST256p.curve: 256//8,
                 ecdsa.NIST384p.curve: 384//8,
                 ecdsa.NIST521p.curve: (521+7)//8,
-                ecdsa.SECP256k1.curve: 256//8}
+                ecdsa.SECP256k1.curve: 256//8,
+                ecdsa.BRAINPOOLP160r1.curve: 160//8,
+                ecdsa.BRAINPOOLP192r1.curve: 192//8,
+                ecdsa.BRAINPOOLP224r1.curve: 224//8,
+                ecdsa.BRAINPOOLP256r1.curve: 256//8,
+                ecdsa.BRAINPOOLP320r1.curve: 320//8,
+                ecdsa.BRAINPOOLP384r1.curve: 384//8,
+                ecdsa.BRAINPOOLP512r1.curve: 512//8}
     if ecdsaAllCurves:
         curveMap[ecdsa.NIST224p.curve] = 224//8
         curveMap[ecdsa.NIST192p.curve] = 192//8
