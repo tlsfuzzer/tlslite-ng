026c50970b95b00c2e7d2cce0e15d52ed22d649a
Hubert Kario <hkario@redhat.com>
2020-11-26 16:30:41 +0100

    add 3.9 testing

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name="tlslite-ng",
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Topic :: Security :: Cryptography',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: System :: Networking'
