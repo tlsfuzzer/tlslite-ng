tomato42/tlslite-1

WIP: Add key logging for TLS 1.3 and TLS 1.2 (#523)

tlslite/session.py
@@ -98,6 +98,8 @@ class Session(object):
         self.tickets = None
         self.tls_1_0_tickets = None
         self.ec_point_format = 0
+        self.cl_handshake_traffic_secret = None
+        self.sr_handshake_traffic_secret = None
 
     def create(self, masterSecret, sessionID, cipherSuite,
                srpUsername, clientCertChain, serverCertChain,
@@ -106,7 +108,9 @@ class Session(object):
                appProto=bytearray(0), cl_app_secret=bytearray(0),
                sr_app_secret=bytearray(0), exporterMasterSecret=bytearray(0),
                resumptionMasterSecret=bytearray(0), tickets=None,
-               tls_1_0_tickets=None, ec_point_format=None):
+               tls_1_0_tickets=None, ec_point_format=None,
+               cl_handshake_traffic_secret=bytearray(0),
+               sr_handshake_traffic_secret=bytearray(0)):
         self.masterSecret = masterSecret
         self.sessionID = sessionID
         self.cipherSuite = cipherSuite
@@ -124,6 +128,8 @@ class Session(object):
         self.sr_app_secret = sr_app_secret
         self.exporterMasterSecret = exporterMasterSecret
         self.resumptionMasterSecret = resumptionMasterSecret
+        self.cl_handshake_traffic_secret = cl_handshake_traffic_secret
+        self.sr_handshake_traffic_secret = sr_handshake_traffic_secret
         # NOTE we need a reference copy not a copy of object here!
         self.tickets = tickets
         self.tls_1_0_tickets = tls_1_0_tickets

unit_tests/test_tlslite_ssl_key_log_file.py
@@ -1,0 +1,112 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import tempfile
+from socket import socket, AF_INET, SOCK_STREAM
+import os
+from typing import Dict, List
+
+import unittest
+
+from tlslite.api import TLSConnection, HandshakeSettings
+from tlslite.errors import TLSRemoteAlert
+
+
+def create_connection(hostname='www.example.com', port=443, settings=HandshakeSettings()):
+    raw_socket = socket(AF_INET, SOCK_STREAM)
+    raw_socket.connect((hostname, port))
+    connection = TLSConnection(raw_socket)
+    connection.handshakeClientCert(settings=settings)
+    return connection
+
+
+def validate_log_file(log_file_name, labels: Dict[str, List[str]]):
+    """
+    TLS1.3 labels
+    ------------------
+    SERVER_HANDSHAKE_TRAFFIC_SECRET <client_random> <secret>
+    EXPORTER_SECRET <client_random> <secret>
+    SERVER_TRAFFIC_SECRET_0  <client_random> <secret>
+    CLIENT_HANDSHAKE_TRAFFIC_SECRET <client_random> <secret>
+    CLIENT_TRAFFIC_SECRET_0 <client_random> <secret>
+
+    TLS1.2 (and below)
+    ----------------
+    CLIENT_RANDOM <client_random> <secret>
+    """
+    with open(log_file_name, 'r') as log_file:
+        for line in log_file.readlines():
+            entry = line.split()
+            label, client_random, secret = entry[0], entry[1], entry[2]
+            if label in labels and labels[label][1] == client_random and labels[label][2] != secret:
+                return False
+
+        return True
+
+
+class TestSslKeyLogFile(unittest.TestCase):
+    def setUp(self):
+        self.temp_log_file = tempfile.NamedTemporaryFile(delete_on_close=False)
+        os.environ['SSLKEYLOGFILE'] = self.temp_log_file.name
+
+    def tearDown(self):
+        # Clean up the temporary file
+        os.remove(self.temp_log_file.name)
+        if os.environ.get('SSLKEYLOGFILE'):
+            del os.environ['SSLKEYLOGFILE']
+
+    def test_tlsv1_1(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3,2)
+        settings.maxVersion = (3,2)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                'CLIENT_RANDOM': connection._clientRandom
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print(f"TLS Remote Alert: {alert}")
+
+
+    def test_tlsv1_2(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3,3)
+        settings.maxVersion = (3,3)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                'CLIENT_RANDOM': connection._clientRandom
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print(f"TLS Remote Alert: {alert}")
+
+    def test_tlsv1_3(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3, 4)
+        settings.maxVersion = (3, 4)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                "SERVER_HANDSHAKE_TRAFFIC_SECRET": connection.session.sr_handshake_traffic_secret,
+                "EXPORTER_SECRET": connection.session.exporterMasterSecret,
+                "SERVER_TRAFFIC_SECRET_0": connection.session.sr_app_secret,
+                "CLIENT_HANDSHAKE_TRAFFIC_SECRET": connection.session.cl_handshake_traffic_secret,
+                "CLIENT_TRAFFIC_SECRET_0": connection.session.cl_app_secret
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print(f"TLS Remote Alert: {alert}")
+
+
+if __name__ == "__main__":
+    unittest.main()

tlsfuzzer/tlslite-ng

tlslite/session.py
@@ -98,6 +98,8 @@ class Session(object):
         self.tickets = None
         self.tls_1_0_tickets = None
         self.ec_point_format = 0
+        self.cl_handshake_traffic_secret = None
+        self.sr_handshake_traffic_secret = None
 
     def create(self, masterSecret, sessionID, cipherSuite,
                srpUsername, clientCertChain, serverCertChain,
@@ -106,7 +108,9 @@ class Session(object):
                appProto=bytearray(0), cl_app_secret=bytearray(0),
                sr_app_secret=bytearray(0), exporterMasterSecret=bytearray(0),
                resumptionMasterSecret=bytearray(0), tickets=None,
-               tls_1_0_tickets=None, ec_point_format=None):
+               tls_1_0_tickets=None, ec_point_format=None,
+               cl_hs_traffic_secret=bytearray(0),
+               sr_hs_traffic_secret=bytearray(0)):
         self.masterSecret = masterSecret
         self.sessionID = sessionID
         self.cipherSuite = cipherSuite
@@ -124,6 +128,8 @@ class Session(object):
         self.sr_app_secret = sr_app_secret
         self.exporterMasterSecret = exporterMasterSecret
         self.resumptionMasterSecret = resumptionMasterSecret
+        self.cl_handshake_traffic_secret = cl_hs_traffic_secret
+        self.sr_handshake_traffic_secret = sr_hs_traffic_secret
         # NOTE we need a reference copy not a copy of object here!
         self.tickets = tickets
         self.tls_1_0_tickets = tls_1_0_tickets

unit_tests/test_tlslite_ssl_key_log_file.py
@@ -1,0 +1,111 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import tempfile
+from socket import socket, AF_INET, SOCK_STREAM
+import os
+
+import unittest
+
+from tlslite.api import TLSConnection, HandshakeSettings
+from tlslite.errors import TLSRemoteAlert
+
+
+def create_connection(hostname='www.example.com', port=443, settings=HandshakeSettings()):
+    raw_socket = socket(AF_INET, SOCK_STREAM)
+    raw_socket.connect((hostname, port))
+    connection = TLSConnection(raw_socket)
+    connection.handshakeClientCert(settings=settings)
+    return connection
+
+
+def validate_log_file(log_file_name, labels):
+    """
+    TLS1.3 labels
+    ------------------
+    SERVER_HANDSHAKE_TRAFFIC_SECRET <client_random> <secret>
+    EXPORTER_SECRET <client_random> <secret>
+    SERVER_TRAFFIC_SECRET_0  <client_random> <secret>
+    CLIENT_HANDSHAKE_TRAFFIC_SECRET <client_random> <secret>
+    CLIENT_TRAFFIC_SECRET_0 <client_random> <secret>
+
+    TLS1.2 (and below)
+    ----------------
+    CLIENT_RANDOM <client_random> <secret>
+    """
+    with open(log_file_name, 'r') as log_file:
+        for line in log_file.readlines():
+            entry = line.split()
+            label, client_random, secret = entry[0], entry[1], entry[2]
+            if label in labels and labels[label][1] == client_random and labels[label][2] != secret:
+                return False
+
+        return True
+
+
+class TestSslKeyLogFile(unittest.TestCase):
+    def setUp(self):
+        self.temp_log_file = tempfile.NamedTemporaryFile(delete=False)
+        os.environ['SSLKEYLOGFILE'] = self.temp_log_file.name
+
+    def tearDown(self):
+        # Clean up the temporary file
+        os.remove(self.temp_log_file.name)
+        if os.environ.get('SSLKEYLOGFILE'):
+            del os.environ['SSLKEYLOGFILE']
+
+    def test_tlsv1_1(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3,2)
+        settings.maxVersion = (3,2)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                'CLIENT_RANDOM': connection._clientRandom
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print("TLS Remote Alert: {0}".format(alert))
+
+
+    def test_tlsv1_2(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3,3)
+        settings.maxVersion = (3,3)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                'CLIENT_RANDOM': connection._clientRandom
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print("TLS Remote Alert: {0}".format(alert))
+
+    def test_tlsv1_3(self):
+        settings = HandshakeSettings()
+        settings.minVersion = (3, 4)
+        settings.maxVersion = (3, 4)
+
+        try:
+            connection = create_connection('www.example.com', 443, settings)
+            expected_labels = {
+                "SERVER_HANDSHAKE_TRAFFIC_SECRET": connection.session.sr_handshake_traffic_secret,
+                "EXPORTER_SECRET": connection.session.exporterMasterSecret,
+                "SERVER_TRAFFIC_SECRET_0": connection.session.sr_app_secret,
+                "CLIENT_HANDSHAKE_TRAFFIC_SECRET": connection.session.cl_handshake_traffic_secret,
+                "CLIENT_TRAFFIC_SECRET_0": connection.session.cl_app_secret
+            }
+            self.assertTrue(validate_log_file(self.temp_log_file.name, expected_labels))
+            connection.close()
+        except TLSRemoteAlert as alert:
+            print("TLS Remote Alert: {0}".format(alert))
+
+
+if __name__ == "__main__":
+    unittest.main()