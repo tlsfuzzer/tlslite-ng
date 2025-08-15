a3dcf55f8db177110c99e045e2a47c8ba7fd648f
Alicja Kario <hkario@redhat.com>
2025-08-15 18:09:46 +0200

    add support for ML-DSA on client side

--- tlslite/utils/keyfactory.py
+++ tlslite/utils/keyfactory.py
@@ -12,6 +12,9 @@ from .python_dsakey import Python_DSAKey
 from .python_eddsakey import Python_EdDSAKey
 from tlslite.utils import cryptomath
 
+if ML_DSA_AVAILABLE:
+    from .python_mldsakey import Python_MLDSAKey
+
 if cryptomath.m2cryptoLoaded:
     from .openssl_rsakey import OpenSSL_RSAKey
 
@@ -249,6 +252,18 @@ def _create_public_eddsa_key(public_key,
     for impl in implementations:
         if impl == "python":
             return Python_EdDSAKey(public_key)
+    raise ValueError("No acceptable implementation")
+
+
+def _create_public_mldsa_key(public_key,
+                             implementations=("python",)):
+    """
+    Convert the dilithium-py public key into concrete implementation of
+    verifier.
+    """
+    for impl in implementations:
+        if impl == "python":
+            return Python_MLDSAKey(public_key)
     raise ValueError("No acceptable implementation")
 
 
